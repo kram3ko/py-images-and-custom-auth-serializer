f9391026b113005a8795c79d4d82aeb988622fcc
Volodymyr Vinohradov <kram3ko@gmail.com>
2025-04-23 08:57:59 +0300

    remove comments in EmailAuthTokenSerializer .

--- user/serializers.py
+++ user/serializers.py
@@ -28,9 +28,6 @@ class EmailAuthTokenSerializer(serializers.Serializer):
                 email=email, password=password
             )
 
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
                 msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg, code="authorization")
